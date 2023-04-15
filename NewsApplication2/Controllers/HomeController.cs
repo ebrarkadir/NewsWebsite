2b3e0d67dd1033d0ac880ae96296e4029048a66d
Ebrar Kadir Çetin <cetinebrarkadir@gmail.com>
2023-04-16 01:11:27 +0300

    First, Last, Prev and next buton added.

--- NewsApplication2/Controllers/HomeController.cs
+++ NewsApplication2/Controllers/HomeController.cs
@@ -11,45 +11,42 @@ using NewsApplication2.Business;
 
 namespace NewsApplication2.Controllers
 {
-	public class HomeController : Controller
-	{
+    public class HomeController : Controller
+    {
 
-		private readonly ILogger<HomeController> _logger;
-		private NewsManager _newsManager = new NewsManager();
+        private readonly ILogger<HomeController> _logger;
+        private NewsManager _newsManager = new NewsManager();
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
-
-        public IActionResult Index(int? page, string saerchNews = "software")
+        public HomeController(ILogger<HomeController> logger)
         {
-            int pageNumber = page ?? 1;
+            _logger = logger;
+        }
 
-            var news = _newsManager.GetNewsOnAPI(DateTime.Today.Date, pageNumber, saerchNews);
+        public IActionResult Index(int page = 1, string saerchNews = "software")
+        {
+            var news = _newsManager.GetNewsOnAPI(DateTime.Today.Date, page, saerchNews);
             var pageCount = _newsManager.GetPageCount();
 
             ViewData["PageCount"] = pageCount;
+            ViewData["ActivePage"] = page;
 
             if (news == null)
             {
                 return NotFound();
             }
-
             return View(news);
+
         }
 
-
-
         public IActionResult Privacy()
-		{
-			return View();
-		}
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
